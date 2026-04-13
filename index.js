f1c8d345e0d4f8b90a3f9724b55c1d0aeb4f8cc4
PapireddySatya <satyapapireddy@gmail.com>
2026-04-13 09:51:17 +0530

    new javascript file

---
+++ index.js
@@ -0,0 +1,93 @@
+// const obj=
+// {
+//     id:1,
+//     name:"user",
+//     myData:[10,"hi",90,"bye"]
+
+// }
+// console.log(obj);
+// obj.loc=hyderabad;
+// console.log(obj.loc);
+// console.log(obj.myData);
+// const users=[
+//     {
+//         id:1,
+//         name:user1,
+//         loc:hyd
+//     },
+//     {
+//         id:2,
+//         nmae:user2,
+//         loc:banglore
+//      }
+// ]
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+// for (const key in object) {
+//     if (!Object.hasOwn(object, key)) continue;
+    
+//     const element = object[key];
+    
+    
+// }
+// function myfun()
+// {
+//     console.log(-1);
+//     return -1;
+// }
+
+// console.log(myfun);
+// console.log(myfun());
+// function add(a,b)
+// {
+//     return a+b;
+// }
+// console.log(add(9,10));
+// function add(a,b)
+// {
+//     return a-b;
+// }
+// console.log(add(9,10));
+
+//anonyms function
+const myFun =function(num1,num2)
+{
+    return num1+num2;
+}
+console.log(myFun(10,10));
+function calculator(a, b, operator)
+{
+    switch(operator)
+    {
+        case  '+':
+             return a+b;
+             break;
+        case '-':
+            return a-b;
+            break;
+        case '*':
+            return a*b;
+            break;
+        case '/':
+            return a/b;
+            break;
+        case '%':
+            return a%b;
+            break;
+        case '^':
+            return Math.pow(a,b);
+            break;
+    }
+
+
+}
+console.log("The addition of Two numbers is "+calculator(10,9,'+'));
+console.log("The subtraction of Two numbers is "+calculator(10,9,'-'));
+console.log("The Multiplication of Two numbers is "+calculator(10,9,'*'));
+console.log("The divison of Two numbers is "+calculator(10,9,'/'));
+console.log("The Modulous of Two numbers is "+calculator(10,9,'%'));
+console.log("The exponential of Two numbers is "+calculator(10,9,'^'));
+
